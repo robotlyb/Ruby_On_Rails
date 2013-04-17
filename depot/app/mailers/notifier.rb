19208632f886bdbf6ff0f50f431ad4d20f203f19
kgtong <kgtong1992@gmail.com>
2013-04-18 00:00:07 +0800

    添加了邮件功能

---
+++ depot/app/mailers/notifier.rb
@@ -0,0 +1,15 @@
+class Notifier < ActionMailer::Base
+  default :from => "kgtong1992@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.order_received.subject
+  #
+  def order_received(order)
+    @order = order
+    @url = products_url(host: 'localhost') 
+    mail :to => "fjfanchenwu@163.com", :subject => "order"
+  end
+
+end
--- depot/config/environments/development.rb
+++ depot/config/environments/development.rb
@@ -34,4 +34,15 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'kgtong1992',
+    :password             =>  "5591271fcw",
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
 end
